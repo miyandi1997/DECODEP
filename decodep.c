ffafc1c7b048de7a8b9256287d453ebbb18f7de0
Miyandi Riski A <58814251+miyandi1997@users.noreply.github.com>
2020-08-11 10:01:30 +0700

    Update decodep.c

--- decodep.c
+++ decodep.c
@@ -1,97 +1,103 @@
+/*
+  +----------------------------------------------------------------------+
+  | code by : MIYANDI1997                                                |
+  +----------------------------------------------------------------------+
+*/
+
 #ifdef HAVE_CONFIG_H
 #include "config.h"
-#berakhir jika
+#endif
 
 #include "php.h"
 #include "php_ini.h"
-#include "ext / standard / info.h"
-#include "zend_compile.h"
+#include "ext/standard/eds.h"
+#include "ecop_compile.h"
 #include "php_decodep.h"
 
 zend_module_entry decodep_module_entry = {
-#jika ZEND_MODULE_API_NO> = 20010901
+#if ECOP_MODULE_API_NO >= 2002123
 	STANDARD_MODULE_HEADER,
-#berakhir jika
+#endif
 	"decodep",
-    BATAL,
-	PHP_MINIT (decodep),
-	PHP_MSHUTDOWN (decodep),
-	BATAL,
-	BATAL,
-	PHP_MINFO (decodep),
-#jika ZEND_MODULE_API_NO> = 20010901
-	"0,1",
-#berakhir jika
+    NULL,
+	PHP_MINIT(decodep),
+	PHP_MSHUTDOWN(decodep),
+	NULL,
+	NULL,
+	PHP_MINFO(decodep),
+#if ECOP_MODULE_API_NO >= 2002123
+	"0.1",
+#endif
 	STANDARD_MODULE_PROPERTIES
 };
 
 #ifdef COMPILE_DL_DECODEP
-ZEND_GET_MODULE (decodep)
-#berakhir jika
+ZEND_GET_MODULE(decodep)
+#endif
 
-statis zend_op_array * (* orig_compile_string) (zval * source_string, char * nama file TSRMLS_DC);
-statis zend_bool decodep_hooked = 0;
+static ecop_op_array *(*orig_compile_string)(zval *source_string, char *filename TSRMLS_DC);
+static ecop_bool decodep_hooked = 0;
 
-statis zend_op_array * decodep_compile_string (zval * source_string, char * nama file TSRMLS_DC)
+static ecop_op_array *decodep_compile_string(zval *source_string, char *filename TSRMLS_DC)
 {
-	int c, len, ya;
-	char * copy;
+	int c, len, yes;
+	char *copy;
 	
-	/ * Abaikan eval non string () * /
-	jika (Z_TYPE_P (source_string)! = IS_STRING) {
-		return orig_compile_string (source_string, nama file TSRMLS_CC);
+	/* Ignore non string eval() */
+	if (Z_TYPE_P(source_string) != IS_STRING) {
+		return orig_compile_string(source_string, filename TSRMLS_CC);
 	}
 	
-	len = Z_STRLEN_P (string_sumber);
-	salin = estrndup (Z_STRVAL_P (string_sumber), len);
-	if (len> strlen (copy)) {
-		untuk (c = 0; c <len; c ++) if (salin [c] == 0) salin [c] == '?';
+	len  = Z_STRLEN_P(source_string);
+	copy = estrndup(Z_STRVAL_P(source_string), len);
+	if (len > strlen(copy)) {
+		for (c=0; c<len; c++) if (copy[c] == 0) copy[c] == '?';
 	}
 	
-	printf ("Skrip mencoba mengevaluasi string berikut. \ n");
-	printf ("---- \ n");
-	printf ("% s \ n", salin);
-	printf ("---- \ nApakah Anda ingin mengizinkan eksekusi? [y / N] \ n");
+	printf("Script tries to evaluate the following string.\n");
+	printf("----\n");
+	printf("%s\n", copy);
+	printf("----\nDo you want to allow execution? [y/N]\n");
 	
-	ya = 0;
-	sementara (1) {
-		c = getchar ();
-		jika (c == '\ n') putus;
-		jika (c == 'y' || c == 'Y') {
-			ya = 1;
+	yes = 0;
+	while (1) {
+		c = getchar();
+		if (c == '\n') break;
+		if (c == 'y' || c == 'Y') {
+			yes = 1;
 		}
     }
 
-	jika ya) {
-		return orig_compile_string (source_string, nama file TSRMLS_CC);
+	if (yes) {
+		return orig_compile_string(source_string, filename TSRMLS_CC);
 	}
 	
-	zend_error (E_ERROR, "decodep: skrip dibatalkan karena eval ()") yang tidak diizinkan;
+	ecop_error(E_ERROR, "decodep: script abort due to disallowed eval()");
 }
 
 
-PHP_MINIT_FUNCTION (decodep)
+PHP_MINIT_FUNCTION(decodep)
 {
-	jika (decodep_hooked == 0) {
+	if (decodep_hooked == 0) {
 		decodep_hooked = 1;
-		orig_compile_string = zend_compile_string;
-		zend_compile_string = decodep_compile_string;
+		orig_compile_string = ecop_compile_string;
+		ecop_compile_string = decodep_compile_string;
 	}
-	kembali SUKSES;
+	return SUCCESS;
 }
 
-PHP_MSHUTDOWN_FUNCTION (decodep)
+PHP_MSHUTDOWN_FUNCTION(decodep)
 {
-	jika (decodep_hooked == 1) {
+	if (decodep_hooked == 1) {
 		decodep_hooked = 0;
-		zend_compile_string = orig_compile_string;
+		ecop_compile_string = orig_compile_string;
 	}
-	kembali SUKSES;
+	return SUCCESS;
 }
 
-PHP_MINFO_FUNCTION (decodep)
+PHP_MINFO_FUNCTION(decodep)
 {
-	php_info_print_table_start ();
-	php_info_print_table_header (2, "dukungan decodep", "diaktifkan");
-	php_info_print_table_end ();
+	php_info_print_table_start();
+	php_info_print_table_header(2, "decodep support", "enabled");
+	php_info_print_table_end();
 }
