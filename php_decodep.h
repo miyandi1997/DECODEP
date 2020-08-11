ea8803c1225b502e6f2f3f2f2e2c2a2240be0beb
Miyandi Riski A <58814251+miyandi1997@users.noreply.github.com>
2020-08-11 10:05:11 +0700

    Update php_decodep.h

--- php_decodep.h
+++ php_decodep.h
@@ -1,12 +1,7 @@
-/*
-  +----------------------------------------------------------------------+
-  | DIPERBARUI OLEH : MIYANDI1997                                                        |
-  +----------------------------------------------------------------------+
-
 #ifndef PHP_DECODEP_H
 #define PHP_DECODEP_H
 
-extern zend_module_entry decodep_module_entry;
+extern ecop_module_entry decodep_module_entry;
 #define phpext_decodep_ptr &decodep_module_entry
 
 #ifdef PHP_WIN32
@@ -26,9 +21,12 @@ PHP_MSHUTDOWN_FUNCTION(decodep);
 PHP_MINFO_FUNCTION(decodep);
 
 #ifdef ZTS
-#define DECODEP_G(v) TSRMG(decodep_globals_id, zend_decodep_globals *, v)
+#define DECODEP_G(v) TSRMG(decodep_globals_id, ecop_decodep_globals *, v)
 #else
 #define DECODEP_G(v) (decodep_globals.v)
 #endif
 
 #endif	/* PHP_DECODEP_H */
+
+
+
